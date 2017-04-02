9acafc67c4ca440bdf1b2162ff188d526088889e
daanaerts <daanaerts@gmail.com>
2017-04-02 12:59:49 +0200

    updated app with express

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 'use strict';
 
-//Lets require/import the HTTP module
-var http = require('http');
+var express = require('express');
+var app = express();
 
-//Lets define a port we want to listen to
-const PORT = 8080;
+app.set('port', (process.env.PORT || 5000));
 
-//We need a function which handles requests and send response
-function handleRequest(request, response) {
-  response.end('It Works!! Path Hit: ' + request.url);
-}
+app.get('/', function(request, response) {
+  response.send('<h1>BATTLESHIP</h1>');
+});
 
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Lets start our server
-server.listen(PORT, function() {
-  //Callback triggered when server is successfully listening. Hurray!
-  console.log("Server listening on: http://localhost:%s", PORT);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
