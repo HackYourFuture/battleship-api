b701411d38f066ebb5064a21122f0bb8a6806184
AhmadKabakibi <ahmadkbakibi@gmail.com>
2017-04-02 15:47:23 +0200

    Fix the port

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.get('/', function(req, res) {
 
 wsController.listen(io);
 
-http.listen(80, function() {
+http.listen(process.env.PORT || 3000;, function() {
   console.log('listening on *:3000');
 });
