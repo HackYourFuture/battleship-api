8432fd4cf4be45e3f805c0bc6841e6fe7ca10b7d
hasanshahoud <hasansh343@gmail.com>
2017-04-02 13:40:21 +0200

    socket.io

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 'use strict';
 
-var express = require('express');
-var app = express();
+var app = require('express')();
+var server = require('http').Server(app);
+server.listen(5000);
+var io = require('socket.io')(server);
 
-app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(request, response) {
-  response.send('<h1>BATTLESHIP</h1>');
+// app.get('/', function (req, res) {
+//   res.sendfile(__dirname + '/index.html');
+// });
+
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+     response.send('<h1>BATTLESHIP</h1>');
+  });
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
